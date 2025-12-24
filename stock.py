82038520cd6e8921297fa30fa9646e93acd5a2cc
Offlinedevc <kunpoonkasadwadthana@gmail.com>
2025-12-25 03:09:09 +0700

    clean

--- stock.py
+++ stock.py
@@ -2390,7 +2390,7 @@ def page_glossary():
 def page_scanner():
     c_t, c_l = st.columns([3, 1])
     with c_t: st.title(get_text('main_title'))
-    with c_l: st.markdown("<br>👉 [**Check out Crypash 🪙**](https://crypash.streamlit.app/)", unsafe_allow_html=True)
+    with c_l: st.markdown("<br>[**Check out Crypash 🪙**](https://crypash.streamlit.app/)", unsafe_allow_html=True)
     st.info(get_text('about_desc'))
 
     # NEW: Market Dashboard
